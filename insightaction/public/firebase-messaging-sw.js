519b4a98e6da2f062e8093e0df6943db52c6fb20
Ashish2271 <ashispandey138c@gmail.com>
2024-08-25 07:01:34 +0530

    maybe fixed?

--- insightaction/public/firebase-messaging-sw.js
+++ insightaction/public/firebase-messaging-sw.js
@@ -1,13 +1,51 @@
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
+// importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
+// importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
-// import { getMessaging } from "firebase/messaging/sw";
-// import { onBackgroundMessage } from "firebase/messaging/sw";
-// import { getMessaging, onMessage } from "firebase/messaging";
-// Initialize the Firebase app in the service worker by passing in
-// your app's Firebase config object.
-// https://firebase.google.com/docs/web/setup#config-object
-const firebaseApp = initializeApp({
+// // import { getMessaging } from "firebase/messaging/sw";
+// // import { onBackgroundMessage } from "firebase/messaging/sw";
+// // import { getMessaging, onMessage } from "firebase/messaging";
+// // Initialize the Firebase app in the service worker by passing in
+// // your app's Firebase config object.
+// // https://firebase.google.com/docs/web/setup#config-object
+// const firebaseApp = initializeApp({
+//   apiKey: "AIzaSyAypjz6KBVcDyzMQipkguq9sgabUvPSfIg",
+//   authDomain: "insightaction-78bd7.firebaseapp.com",
+//   projectId: "insightaction-78bd7",
+//   storageBucket: "insightaction-78bd7.appspot.com",
+//   messagingSenderId: "141002890917",
+//   appId: "1:141002890917:web:8bbab018a618c47035f74c",
+//   measurementId: "G-2EWCKCS0CN",
+// });
+
+// // Retrieve an instance of Firebase Messaging so that it can handle background
+// // messages.
+
+
+
+
+// const messaging = getMessaging();
+// onBackgroundMessage(messaging, (payload) => {
+//   console.log('[firebase-messaging-sw.js] Received background message ', payload);
+//   // Customize notification here
+//   const notificationTitle = 'Background Message Title';
+//   const notificationOptions = {
+//     body: 'Background Message body.',
+//     icon: '/firebase-logo.png'
+//   };
+
+//   self.registration.showNotification(notificationTitle,
+//     notificationOptions);
+// });
+
+
+
+importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
+importScripts(
+  "https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js"
+);
+
+// Replace these with your own Firebase config keys...
+const firebaseConfig = {
   apiKey: "AIzaSyAypjz6KBVcDyzMQipkguq9sgabUvPSfIg",
   authDomain: "insightaction-78bd7.firebaseapp.com",
   projectId: "insightaction-78bd7",
@@ -15,24 +53,57 @@ const firebaseApp = initializeApp({
   messagingSenderId: "141002890917",
   appId: "1:141002890917:web:8bbab018a618c47035f74c",
   measurementId: "G-2EWCKCS0CN",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log(
+    "[firebase-messaging-sw.js] Received background message ",
+    payload
+  );
+
+  // payload.fcmOptions?.link comes from our backend API route handle
+  // payload.data.link comes from the Firebase Console where link is the 'key'
+  const link = payload.fcmOptions?.link || payload.data?.link;
+
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: "./logo.png",
+    data: { url: link },
+  };
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
 
-// Retrieve an instance of Firebase Messaging so that it can handle background
-// messages.
+self.addEventListener("notificationclick", function (event) {
+  console.log("[firebase-messaging-sw.js] Notification click received.");
 
+  event.notification.close();
 
+  // This checks if the client is already open and if it is, it focuses on the tab. If it is not open, it opens a new tab with the URL passed in the notification payload
+  event.waitUntil(
+    clients
+      // https://developer.mozilla.org/en-US/docs/Web/API/Clients/matchAll
+      .matchAll({ type: "window", includeUncontrolled: true })
+      .then(function (clientList) {
+        const url = event.notification.data.url;
 
+        if (!url) return;
 
-const messaging = getMessaging();
-onBackgroundMessage(messaging, (payload) => {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  // Customize notification here
-  const notificationTitle = 'Background Message Title';
-  const notificationOptions = {
-    body: 'Background Message body.',
-    icon: '/firebase-logo.png'
-  };
+        // If relative URL is passed in firebase console or API route handler, it may open a new window as the client.url is the full URL i.e. https://example.com/ and the url is /about whereas if we passed in the full URL, it will focus on the existing tab i.e. https://example.com/about
+        for (const client of clientList) {
+          if (client.url === url && "focus" in client) {
+            return client.focus();
+          }
+        }
 
-  self.registration.showNotification(notificationTitle,
-    notificationOptions);
+        if (clients.openWindow) {
+          console.log("OPENWINDOW ON CLIENT");
+          return clients.openWindow(url);
+        }
+      })
+  );
 });
