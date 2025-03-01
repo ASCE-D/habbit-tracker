3d87143785932595f7bdcf9cde11cc66376688a2
asce-d <deepeshgenani@gmail.com>
2025-03-01 21:12:07 +0530

    build fix

--- insightaction/src/app/(dashboard)/journal/test/page.tsx
+++ insightaction/src/app/(dashboard)/journal/test/page.tsx
@@ -1,26 +1,7 @@
-"use client";
-
-import { useState } from "react";
-import SimpleHabitList from "@/components/Dashboard/DragTest";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-export default function Page() {
-  const [date, setDate] = useState(new Date());
-
+const page = () => {
   return (
-    <div className="App">
-      <h1>Simple Draggable Habit List</h1>
-
-      {/* DatePicker to select the date */}
-      <DatePicker
-        selected={date}
-        //@ts-ignore
-        onChange={(date: Date) => setDate(date)}
-        inline
-      />
-
-      {/* Pass the selected date to SimpleHabitList */}
-      <SimpleHabitList date={date} onClose={()=>{}}/>
-    </div>
-  );
+    <div>page</div>
+  )
 }
+
+export default page
