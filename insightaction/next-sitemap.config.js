cceed2eb275581a3e23437f8ba2c8280e6832c7e
asce-d <deepeshgenani@gmail.com>
2025-04-28 00:22:26 +0530

    seo sitemap

---
+++ insightaction/next-sitemap.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://insightaction.pureapps.tech/',
+  generateRobotsTxt: true,
+  // optional configurations
+  changefreq: 'daily',
+  priority: 0.7,
+  // exclude paths if needed
+  // exclude: ['/admin/*', '/private/*'],
+}
